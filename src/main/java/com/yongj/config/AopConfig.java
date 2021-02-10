191d29f9777af73b19bd96dea22a221974ce91ed
CurtisNewbie <zhuangyongj@gmail.com>
2021-02-10 15:48:42 +0800

    Add AOP for logging

---
+++ src/main/java/com/yongj/config/AopConfig.java
@@ -0,0 +1,13 @@
+package com.yongj.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @author yongjie.zhuang
+ */
+@Configuration
+@EnableAspectJAutoProxy
+public class AopConfig {
+
+}
---
+++ src/main/java/com/yongj/web/LogAspect.java
@@ -0,0 +1,58 @@
+package com.yongj.web;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+/**
+ * @author yongjie.zhuang
+ */
+@Aspect
+@Component
+public class LogAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(LogAspect.class);
+
+    @Around("execution(* com.yongj.web.FileController.*(..))")
+    public void logBeforeOperation(ProceedingJoinPoint pjp) throws Throwable {
+        StopWatch sw = new StopWatch();
+        try {
+            sw.start();
+            pjp.proceed();
+        } finally {
+            sw.stop();
+            logger.info("JoinPoint: '{}', arguments: '{}', took '{}' seconds", pjp.toShortString(), cvtToStr(pjp.getArgs()), sw.getTotalTimeSeconds());
+        }
+    }
+
+    private static final String cvtToStr(Object[] args) {
+        if (args == null)
+            return "[ null ]";
+
+        StringBuilder sb = new StringBuilder();
+        for (Object o : args) {
+            if (isPrimitiveType(o)) {
+                if (sb.length() > 0)
+                    sb.append(", ");
+                sb.append(o == null ? "null" : o.toString());
+            }
+        }
+        sb.insert(0, "[ ");
+        sb.append(" ]");
+        return sb.toString();
+    }
+
+    private static final boolean isPrimitiveType(Object o) {
+        return o == null
+                || o instanceof String
+                || o instanceof Integer
+                || o instanceof Short
+                || o instanceof Long
+                || o instanceof Double
+                || o instanceof Float;
+    }
+}
