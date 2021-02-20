2fc790127fdeccecf7cd432ee2bb1948c5b92ef4
CurtisNewbie <zhuangyongj@gmail.com>
2021-02-20 18:04:28 +0800

    Update docker-build.sh for mounting

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,6 @@
 ./build.sh
 docker build . -t file-server:latest
-# add -v ~/some/folder/to/mount:/home/file/server for mounting
+# add '-v ~/some/folder/to/mount:/home/file/server' before 'file-server:latest' for mounting
 docker run --name file-server-app -p 8080:8080 file-server:latest
 
 
