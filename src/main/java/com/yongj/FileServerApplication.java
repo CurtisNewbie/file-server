2e85949862cc5ef916097baa1392cce7face0b40
CurtisNewbie <zhuangyongj@gmail.com>
2021-04-04 17:16:25 +0800

    Adopt authmodule

--- src/main/java/com/yongj/FileServerApplication.java
+++ src/main/java/com/yongj/FileServerApplication.java
@@ -2,14 +2,16 @@ package com.yongj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ComponentScan(basePackages = "com.curtisnewbie.module")
 public class FileServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FileServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FileServerApplication.class, args);
+    }
 
 }
