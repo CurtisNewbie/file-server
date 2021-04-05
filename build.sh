30cac124ebbddf4125b01ee88d2c2c5d9d4c20c6
CurtisNewbie <zhuangyongj@gmail.com>
2021-04-05 11:58:27 +0800

    Update build script

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+if [ -d src/main/resources/static/ ]; then
+    rm -r src/main/resources/static/
+fi
+
 mkdir src/main/resources/static/  
+
 cp -r frontend/* src/main/resources/static/  
+
 mvn clean package 
-# rm -r src/main/resources/static/  
+
+rm -r src/main/resources/static/  
+
 cp ./**/*.jar .
 
