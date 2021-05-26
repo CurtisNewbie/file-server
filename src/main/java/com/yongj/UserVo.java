1f33f6b2666e6f124a7227441bb87c5d9f85e690
CurtisNewbie <zhuangyongj@gmail.com>
2021-05-26 21:44:16 +0800

    Display user info

---
+++ src/main/java/com/yongj/UserVo.java
@@ -0,0 +1,33 @@
+package com.yongj;
+
+/**
+ * @author yongjie.zhuang
+ */
+public class UserVo {
+
+    /**
+     * username
+     */
+    private String username;
+
+    /**
+     * role
+     */
+    private String role;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
--- src/main/java/com/yongj/web/UserController.java
+++ src/main/java/com/yongj/web/UserController.java
@@ -2,38 +2,57 @@ package com.yongj.web;
 
 import com.curtisnewbie.module.auth.consts.UserRole;
 import com.curtisnewbie.module.auth.dao.RegisterUserDto;
+import com.curtisnewbie.module.auth.dao.UserEntity;
 import com.curtisnewbie.module.auth.exception.ExceededMaxAdminCountException;
 import com.curtisnewbie.module.auth.exception.UserRegisteredException;
 import com.curtisnewbie.module.auth.services.api.UserService;
 import com.curtisnewbie.module.auth.vo.RegisterUserVo;
+import com.yongj.UserVo;
+import com.yongj.dto.Resp;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author yongjie.zhuang
  */
 @RestController
-@RequestMapping("/user/register")
-public class RegisterController {
+@RequestMapping("/user")
+public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PreAuthorize("hasAuthority('admin')")
-    @PostMapping("/guest")
-    public ResponseEntity<?> guestRegister(@RequestBody RegisterUserVo registerUserVo) throws UserRegisteredException,
+    @PostMapping("/register/guest")
+    public Resp<?> guestRegister(@RequestBody RegisterUserVo registerUserVo) throws UserRegisteredException,
             ExceededMaxAdminCountException {
         RegisterUserDto dto = new RegisterUserDto();
         BeanUtils.copyProperties(registerUserVo, dto);
         dto.setRole(UserRole.GUEST.val);
         userService.register(dto);
-        return ResponseEntity.ok().build();
+        return Resp.ok();
+    }
+
+    @GetMapping("/info")
+    public Resp<UserVo> getUserInfo() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null)
+            return Resp.error("Please login first");
+        if (auth.getPrincipal() == null || !(auth.getPrincipal() instanceof UserEntity))
+            throw new IllegalStateException("Authentication#principal is null or not instance of UserEntity");
+        UserEntity ue = UserEntity.class.cast(auth.getPrincipal());
+        return Resp.of(toUserVo(ue));
+    }
+
+    private UserVo toUserVo(UserEntity ue) {
+        UserVo uv = new UserVo();
+        uv.setUsername(ue.getUsername());
+        uv.setRole(ue.getRole());
+        return uv;
     }
 
 //
