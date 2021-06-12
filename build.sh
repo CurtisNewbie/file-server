e9a68feb62576fdc1393ab6bf258e9d9dcb9032f
CurtisNewbie <zhuangyongj@gmail.com>
2021-06-12 23:56:47 +0800

    Update build.sh script

--- build.sh
+++ build.sh
@@ -6,11 +6,18 @@ fi
 
 mkdir src/main/resources/static/  
 
-cp -r frontend/vanilla/* src/main/resources/static/  
+# build angular app, and copy the build to resources/static folder
+(
+    cd frontend/angular/file-server-front/; 
+    ng build;
+    cp -r dist/file-server/* ../../../src/main/resources/static/;
+)
+
+# cp -r frontend/vanilla/* src/main/resources/static/  
 
 mvn clean package 
 
 rm -r src/main/resources/static/  
 
-cp ./**/*.jar .
+cp ./target/file-server-0.0.1.jar .
 
