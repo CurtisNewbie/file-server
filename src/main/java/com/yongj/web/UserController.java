f659b059b79ce200eb4868b59adcacb6ee15a118
CurtisNewbie <zhuangyongj@gmail.com>
2021-06-26 00:44:23 +0800

    Impl func to change password

--- src/main/java/com/yongj/web/UserController.java
+++ src/main/java/com/yongj/web/UserController.java
@@ -18,7 +18,6 @@ import com.yongj.vo.UserVo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -43,14 +42,14 @@ public class UserController {
             MsgEmbeddedException {
         RegisterUserVo dto = new RegisterUserVo();
         BeanUtils.copyProperties(registerUserVo, dto);
+
         // validate whether username and password is entered
-        if (!StringUtils.hasText(dto.getUsername()))
-            return Result.error("Please enter username");
-        if (!StringUtils.hasText(dto.getPassword()))
-            return Result.error("Please enter password");
+        ValidUtils.requireNotEmpty(dto.getUsername(), "Please enter username");
+        ValidUtils.requireNotEmpty(dto.getPassword(), "Please enter password");
+
         // validate if the username and password is the same
-        if (Objects.equals(dto.getUsername(), dto.getPassword()))
-            return Result.error("Username and password must be different");
+        ValidUtils.requireNotEquals(dto.getUsername(), dto.getPassword(), "Username and password must be different");
+
         // validate if the password is too short
         if (dto.getPassword().length() < PASSWORD_LENGTH)
             return Result.error("Password must have at least " + PASSWORD_LENGTH + "characters");
@@ -105,13 +104,17 @@ public class UserController {
         return Result.of(BeanCopyUtils.toType(ue, UserVo.class));
     }
 
-    // TODO, not supported so far
-//    @PostMapping("/password/update")
+    @PostMapping("/password/update")
     public Result<Void> updatePassword(@RequestBody UpdatePasswordVo vo) throws MsgEmbeddedException {
-        ValidUtils.requireNonNull(vo.getNewPassword());
-        ValidUtils.requireNonNull(vo.getPrevPassword());
-        if (Objects.equals(vo.getNewPassword(), vo.getPrevPassword()))
-            return Result.error("New password must be different");
+        ValidUtils.requireNotEmpty(vo.getNewPassword());
+        ValidUtils.requireNotEmpty(vo.getPrevPassword());
+
+        // check if the old password and prev password are equal
+        ValidUtils.requireNotEquals(vo.getNewPassword(), vo.getPrevPassword(), "New password must be different");
+
+        // validate if the new password is too short
+        if (vo.getNewPassword().length() < PASSWORD_LENGTH)
+            return Result.error("Password must have at least " + PASSWORD_LENGTH + "characters");
 
         UserEntity ue = AuthUtil.getUserEntity();
         try {
@@ -129,15 +132,4 @@ public class UserController {
         }).map(ui -> BeanCopyUtils.toType(ui, UserInfoFsVo.class))
                 .collect(Collectors.toList());
     }
-
-//
-//    @PostMapping("/admin")
-//    public ResponseEntity<?> adminRegister(RegisterUserVo registerUserVo) throws UserRegisteredException,
-//            ExceededMaxAdminCountException {
-//        RegisterUserDto dto = new RegisterUserDto();
-//        BeanUtils.copyProperties(registerUserVo, dto);
-//        dto.setRole(Role.ADMIN.val);
-//        userService.register(dto);
-//        return ResponseEntity.ok().build();
-//    }
 }
