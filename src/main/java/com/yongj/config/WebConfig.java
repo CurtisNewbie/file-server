5b1a25c0084f7d4bc71b3671376bb75a49ac3add
CurtisNewbie <zhuangyongj@gmail.com>
2021-07-18 15:38:07 +0800

    Enable cache-control for static resources

---
+++ src/main/java/com/yongj/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.yongj.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Configuration of Web MVC
+ *
+ * @author yongjie.zhuang
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private static final int RESOURCES_CACHE_MAX_AGE_MINUTES = 10;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // cache 10 minutes for the static resources
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(RESOURCES_CACHE_MAX_AGE_MINUTES, TimeUnit.MINUTES));
+    }
+}
