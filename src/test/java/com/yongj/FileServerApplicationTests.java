e6dd09d8f982dc91b2e6dcdacd86172f58d596d5
CurtisNewbie <zhuangyongj@gmail.com>
2021-07-27 18:24:53 +0800

    Disable context test, nacos may not be available during the test

--- src/test/java/com/yongj/FileServerApplicationTests.java
+++ src/test/java/com/yongj/FileServerApplicationTests.java
@@ -1,8 +1,10 @@
 package com.yongj;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class FileServerApplicationTests {
 
