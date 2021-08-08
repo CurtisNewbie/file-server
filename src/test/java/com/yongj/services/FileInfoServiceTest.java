1708cda90b5bb20cd2908037ccd45d56857c432e
CurtisNewbie <zhuangyongj@gmail.com>
2021-08-08 21:09:30 +0800

    Fix test cases

--- src/test/java/com/yongj/services/FileInfoServiceTest.java
+++ src/test/java/com/yongj/services/FileInfoServiceTest.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -33,6 +34,7 @@ import java.util.zip.ZipFile;
 @Slf4j
 @SpringBootTest
 @Rollback
+@Transactional
 public class FileInfoServiceTest {
 
     private static final String TEST_DOWNLOADED_FILE = "downloaded-file";
