7ae047c646a4217ab219d046f2b1f2a471e43c60
CurtisNewbie <zhuangyongj@gmail.com>
2021-08-09 15:06:36 +0800

    Add test build script

---
+++ test-bundled-build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -d src/main/resources/static/ ]; then
+    rm -r src/main/resources/static/
+fi
+
+mkdir src/main/resources/static/  
+
+echo "Build angular? [y/Y]"
+
+read ans
+
+if [ $ans == "y" ] || [ $ans == "Y" ]
+then
+    (
+        cd frontend/angular/file-server-front/; 
+        ng build;
+    )
+fi
+
+
+(
+    cd frontend/angular/file-server-front/; 
+    cp -r dist/file-server/* ../../../src/main/resources/static/;
+)
+
+mvn clean package 
+
+rm -r src/main/resources/static/  
+
+scp "target/file-server-0.0.1.jar" "zhuangyongj@192.168.10.128:~/exec/fileserver.jar"
