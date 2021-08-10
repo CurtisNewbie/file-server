36ce89bce5ef59736658708adcd8a981ae295a2e
CurtisNewbie <zhuangyongj@gmail.com>
2021-08-10 18:00:24 +0800

    Change redisson config

--- src/main/java/com/yongj/FileServerApplication.java
+++ src/main/java/com/yongj/FileServerApplication.java
@@ -2,21 +2,24 @@ package com.yongj;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
+import org.springframework.context.annotation.*;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+import java.io.IOException;
 
 @EnableRedisHttpSession
 @PropertySources({
         @PropertySource("classpath:dubbo.properties"),
         @PropertySource("classpath:application.properties"),
-        @PropertySource("classpath:common.properties")
+        @PropertySource("classpath:common.properties"),
 })
 @EnableDubbo
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -34,4 +37,13 @@ public class FileServerApplication {
     static class AuthModuleConfig {
     }
 
+    @Value("${redisson-config}")
+    private String redissonConfig;
+
+    @Bean
+    public RedissonClient redissonClient() throws IOException {
+        Config config = Config.fromYAML(this.getClass().getClassLoader().getResourceAsStream(redissonConfig));
+        RedissonClient redisson = Redisson.create(config);
+        return redisson;
+    }
 }
