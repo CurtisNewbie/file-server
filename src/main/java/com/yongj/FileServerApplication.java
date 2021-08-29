a83346c3cc42e6dc1de35ad24749f4c8bbc3a770
CurtisNewbie <zhuangyongj@gmail.com>
2021-08-29 17:35:45 +0800

    Add namespace for redis http session

--- src/main/java/com/yongj/FileServerApplication.java
+++ src/main/java/com/yongj/FileServerApplication.java
@@ -14,7 +14,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 
 import java.io.IOException;
 
-@EnableRedisHttpSession
+@EnableRedisHttpSession(redisNamespace = "file-service")
 @PropertySources({
         @PropertySource("classpath:dubbo.properties"),
         @PropertySource("classpath:application.properties"),
