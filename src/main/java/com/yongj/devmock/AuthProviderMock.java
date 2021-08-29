3f87115b0eb2d8eb5390a9986f86b1a64826d4ac
CurtisNewbie <zhuangyongj@gmail.com>
2021-08-29 23:12:17 +0800

    Mock bean & config for dev profile

---
+++ src/main/java/com/yongj/devmock/AuthProviderMock.java
@@ -0,0 +1,49 @@
+package com.yongj.devmock;
+
+import com.curtisnewbie.module.auth.config.AuthProvider;
+import com.curtisnewbie.service.auth.remote.consts.UserRole;
+import com.curtisnewbie.service.auth.remote.vo.UserVo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+/**
+ * Mock bean that replaces {@link com.curtisnewbie.module.auth.config.AuthProvider} for dev profile
+ *
+ * @author yongjie.zhuang
+ */
+@Slf4j
+@Profile("dev")
+@Component
+@Primary
+public class AuthProviderMock extends AuthProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        if (authentication.isAuthenticated())
+            return authentication;
+
+        UserVo uv = new UserVo();
+        uv.setId(3);
+        uv.setUsername("zhuangyongj");
+        uv.setRole(UserRole.ADMIN.getValue());
+
+        log.info("Dev profile mock - authenticated user: {}", uv.toString());
+
+        return new UsernamePasswordAuthenticationToken(uv,
+                "123456",
+                Arrays.asList(new SimpleGrantedAuthority(uv.getRole())));
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return true;
+    }
+}
