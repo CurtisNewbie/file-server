1f52e3c5dd93f77874ccd5a3e2aa4cf986abead0
yongj.zhuang <zhuangyongj@gmail.com>
2022-08-30 00:06:23 +0800

    @JsonFormat for LDT

--- file-server/src/main/java/com/yongj/vo/VFolderListResp.java
+++ file-server/src/main/java/com/yongj/vo/VFolderListResp.java
@@ -1,9 +1,11 @@
 package com.yongj.vo;
 
-import com.yongj.enums.*;
-import lombok.*;
+import com.curtisnewbie.common.util.DateUtils;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.yongj.enums.VFOwnership;
+import lombok.Data;
 
-import java.time.*;
+import java.time.LocalDateTime;
 
 /**
  * Virtual folder
@@ -16,12 +18,14 @@ public class VFolderListResp {
     private Integer id;
 
     /** when the record is created */
+    @JsonFormat(pattern = DateUtils.DD_MM_YYYY_HH_MM)
     private LocalDateTime createTime;
 
     /** who created this record */
     private String createBy;
 
     /** when the record is updated */
+    @JsonFormat(pattern = DateUtils.DD_MM_YYYY_HH_MM)
     private LocalDateTime updateTime;
 
     /** who updated this record */
