d120f22691206a5ed86ba8a2c74cf13a25bc0b77
yongj.zhuang <zhuangyongj@gmail.com>
2022-10-04 21:51:54 +0800

    Modified:  pom.xml

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
 FROM openjdk:17-jdk-alpine
+
 LABEL author="yongjie.zhuang"
 LABEL descrption="File Service"
+ENV TZ=Asia/Shanghai
+
 COPY . /usr/src/file-service
 WORKDIR /usr/src/file-service
+
 EXPOSE 8080/tcp
 EXPOSE 7010/tcp
+
 CMD ["java", "-jar", "file-server-1.0.0.jar", "--spring.profiles.active=test"]
--- file-server/src/main/java/com/yongj/config/ObjectMapperConfig.java
+++ file-server/src/main/java/com/yongj/config/ObjectMapperConfig.java
@@ -2,7 +2,6 @@ package com.yongj.config;
 
 import com.curtisnewbie.common.util.JsonUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
